8dcba2dc9fa2782df56c292590ab3a876fcd45a7
phpunion <huangtuo@zmeng123.com>
2016-02-23 12:05:25 +0800

    语音测试

--- PigCms/Lib/Action/Wap/TestAction.class.php
+++ PigCms/Lib/Action/Wap/TestAction.class.php
@@ -17,8 +17,8 @@ class TestAction extends WapAction{
 
 
     public function sound(){
-
-        $this->display();
+        echo "111";
+        //$this->display();
     }
 	//判断关注
 	public function gzhurl(){
