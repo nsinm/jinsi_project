56ee4e185469ea216bb79412aea8bf5089d3edcc
shiyingjie <shiyingjie@zmeng123.com>
2016-02-24 11:32:50 +0800

    增加直播室功能架构

---
+++ Conf/Telecast/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nsinm
+ * Date: 16/2/24
+ * Time: 上午11:00
+ */
+
+return array(
+    'TEMP_FILE_DEPR' => '_',
+    'DEFAULT_THEME'=>'default',
+);
---
+++ PigCms/Lib/Action/Telecast/IndexAction.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * 直播频道主页控制器类
+ *
+ * Created by PhpStorm.
+ * User: Nsinm
+ * Date: 16/2/24
+ * Time: 上午11:06
+ */
+
+class IndexAction extends Action
+{
+    public function index()
+    {
+        $this->display();
+    }
+}
