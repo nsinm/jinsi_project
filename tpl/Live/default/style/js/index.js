0d0cdd6d3a1bc80e8cc29ba66f9a526044e34ff2
shiyingjie <shiyingjie@zmeng123.com>
2016-02-27 16:22:55 +0800

    test

--- tpl/Live/default/style/js/index.js
+++ tpl/Live/default/style/js/index.js
@@ -3,11 +3,14 @@
  */
 
 var indexAction = {
-     'getRecomendInstructor' : function(){
-
-     },
+    'getRecomendInstructor' : function(){
+        $.getJSON(params.griUrl, {}, function(data){
+            alert(data);
+        }, 'JSON');
+    },
 };
 
 $(function(){
-
+    //获取推荐导师列表
+    indexAction.getRecomendInstructor();
 });
