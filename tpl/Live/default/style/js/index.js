d6e0333babe50a6767dbb06ed83ccc32198f131a
shiyingjie <shiyingjie@zmeng123.com>
2016-02-27 17:23:30 +0800

    获取推荐导师

--- tpl/Live/default/style/js/index.js
+++ tpl/Live/default/style/js/index.js
@@ -5,7 +5,18 @@
 var indexAction = {
     'getRecomendInstructor' : function(){
         $.getJSON(params.griUrl, {}, function(data){
-            alert(data);
+            var tag = $('.bd .user_grids');
+            tag.remove();
+            var html = '';
+            for(var instructor in data){
+                html += '<a href="javascript:;" class="weui_grid js_grid col-3-md" data-id="button">';
+                html += '<div class="user_thumb mb10">';
+                html += '<img src="/tpl/Live/default/style/uinfo/1.jpg" alt="">';
+                html += '</div>';
+                html += '<p class="weui_grid_label">' + instructor.jinsi_user_name + '</p>';
+                html += '</a>';
+            }
+            tag.html(html);
         }, 'JSON');
     },
 };
