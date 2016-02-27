ef6fb5dba27a5633c5f64c8716bccb368e82ebcf
shiyingjie <shiyingjie@zmeng123.com>
2016-02-27 17:38:17 +0800

    test

--- tpl/Live/default/style/js/index.js
+++ tpl/Live/default/style/js/index.js
@@ -5,15 +5,15 @@
 var indexAction = {
     'getRecomendInstructor' : function(){
         $.getJSON(params.griUrl, {}, function(data){
-            var tag = $('.bd .user_grids');
+            var tag = $('.bd .weui_grids');
             tag.remove();
             var html = '';
-            for(var instructor in data){
+            for(var index in data){
                 html += '<a href="javascript:;" class="weui_grid js_grid col-3-md" data-id="button">';
                 html += '<div class="user_thumb mb10">';
                 html += '<img src="/tpl/Live/default/style/uinfo/1.jpg" alt="">';
                 html += '</div>';
-                html += '<p class="weui_grid_label">' + instructor.jinsi_user_name + '</p>';
+                html += '<p class="weui_grid_label">' + data[index].jinsi_user_name + '</p>';
                 html += '</a>';
             }
             tag.html(html);
