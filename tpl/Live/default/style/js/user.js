163e2fb2ec531ba479a22aad2945156c58ba8451
shiyingjie <shiyingjie@zmeng123.com>
2016-02-29 18:19:53 +0800

    test

--- tpl/Live/default/style/js/user.js
+++ tpl/Live/default/style/js/user.js
@@ -4,6 +4,24 @@
 
 var userAction = {
     'getUserInfo' : function(){
-
+        $.getJSON(params.guiUrl, {}, function(data){
+            if(data.errcode == '0'){
+                var infos = data.data[0];
+                $('.user_thumb img').attr('src', infos.jinsi_user_header_pic);
+                $('#name').text(infos.jinsi_user_name);
+                $('#style').text(infos.jinsi_user_style);
+                $('#sign').text(infos.jinsi_user_sign);
+                $('#info').text(infos.jinsi_user_info);
+            }else{
+                alert(data.msg);
+            }
+        }, 'JSON');
+    },
+    'init' : function(){
+        this.getUserInfo();
     }
 };
+
+$(function(){
+    userAction.init();
+})
