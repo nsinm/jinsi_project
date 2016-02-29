34c06cb9486f26c72fc36063be71dd6ffe744851
shiyingjie <shiyingjie@zmeng123.com>
2016-02-29 17:17:53 +0800

    test

--- tpl/Live/default/style/js/index.js
+++ tpl/Live/default/style/js/index.js
@@ -9,7 +9,7 @@ var indexAction = {
             var html = '';
             if(data.errcode == 0) {
                 for (var index in data.data) {
-                    html += '<a href="javascript:;" class="weui_grid js_grid col-3-md" data-id="button" data-uid="' + data.data[index].id + '">';
+                    html += '<a href="javascript:;" class="weui_grid js_grid col-3-md" data-id="button" onclick="goUserInfo(this, ' + data.data[index].id + ')">';
                     html +=     '<div class="user_thumb mb10">';
                     html +=         '<img src="' + data.data[index].jinsi_user_header_pic + '" alt="">';
                     html +=     '</div>';
@@ -19,7 +19,7 @@ var indexAction = {
             }else{
                 html += '还没有推荐导师哦';
             }
-            tag.append(html);
+            tag.html(html);
         }, 'JSON');
     },
     'getReInComment' : function(){
@@ -69,11 +69,6 @@ var indexAction = {
             }
             tag.append(html);
         }, 'JSON');
-    },
-    'getUserInfo' : function(){
-        $('.weui_grid').each(function(){
-            console.log($(this));
-        });
     }
 };
 
@@ -82,6 +77,4 @@ $(function(){
     indexAction.getRecomendInstructor();
     //获取关注导师评论列表
     indexAction.getReInComment();
-    //跳转到用户信息页面
-    indexAction.getUserInfo();
 });
