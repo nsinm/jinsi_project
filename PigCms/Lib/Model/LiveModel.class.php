14545dcd82762b3636a632c35bcc47cd7c3ce78c
phpunion <huangtuo@zmeng123.com>
2016-02-29 16:05:01 +0800

    获取用的数据信息，如果数据库中存在不在获取

--- PigCms/Lib/Model/LiveModel.class.php
+++ PigCms/Lib/Model/LiveModel.class.php
@@ -63,6 +63,7 @@ class LiveModel extends Model
         $where['openid'] = $openid;
         $user = M('user');
         $user_info = $user->where($where)->find();
+        echo $user->getlastsql();
         //print_r($user_info);
         if($user_info){
             print_r($user_info);
