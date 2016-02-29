aa3259504af632b668abd311af22a13ec2150a0e
shiyingjie <shiyingjie@zmeng123.com>
2016-02-29 13:58:35 +0800

    test

---
+++ PigCms/Lib/Action/Live/UserAction.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nsinm
+ * Date: 16/2/29
+ * Time: 下午1:02
+ */
+
+class UserAction extends Action
+{
+    public function index ()
+    {
+        $this->display();
+    }
+}
