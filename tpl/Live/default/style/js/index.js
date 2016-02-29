5b2b60cdbbc784eb77b874758fcb4f42d440195d
shiyingjie <shiyingjie@zmeng123.com>
2016-02-29 17:15:37 +0800

    test

--- tpl/Live/default/style/js/index.js
+++ tpl/Live/default/style/js/index.js
@@ -9,7 +9,7 @@ var indexAction = {
             var html = '';
             if(data.errcode == 0) {
                 for (var index in data.data) {
-                    html += '<a href="javascript:;" class="weui_grid js_grid col-3-md" data-id="button" onclick="goUserInfo(this, ' + data.data[index].id + ')">';
+                    html += '<a href="javascript:;" class="weui_grid js_grid col-3-md" data-id="button" data-uid="' + data.data[index].id + '">';
                     html +=     '<div class="user_thumb mb10">';
                     html +=         '<img src="' + data.data[index].jinsi_user_header_pic + '" alt="">';
                     html +=     '</div>';
@@ -69,6 +69,11 @@ var indexAction = {
             }
             tag.append(html);
         }, 'JSON');
+    },
+    'getUserInfo' : function(){
+        $('.weui_grid').each(function(){
+            console.log($(this));
+        });
     }
 };
 
@@ -77,4 +82,6 @@ $(function(){
     indexAction.getRecomendInstructor();
     //获取关注导师评论列表
     indexAction.getReInComment();
+    //跳转到用户信息页面
+    indexAction.getUserInfo();
 });
