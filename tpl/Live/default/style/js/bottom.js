ca9e29db9101ff49fa7be6b2f81fc0cb415aff24
shiyingjie <shiyingjie@zmeng123.com>
2016-03-01 12:30:28 +0800

    test

--- tpl/Live/default/style/js/bottom.js
+++ tpl/Live/default/style/js/bottom.js
@@ -4,12 +4,13 @@
 
 $(function(){
     $('.bottom-bar').find('div').each(function(i){
-        alert(i);
+        if($('.bottom-bar').find('div').get(i).getAttribute('class') == 'selected'){
+            $('.bottom-bar').find('div').get(i).setAttribute('class', null);
+        }
         $(this).click(function(){
             if($(this).attr('class') == 'selected')
                 return;
-
+            $(this).attr('class', 'selected');
         })
-
     });
 })
