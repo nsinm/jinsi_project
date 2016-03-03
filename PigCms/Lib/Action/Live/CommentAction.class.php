8239f58e9c14992a7536e68a34865af7e9b573ae
shiyingjie <shiyingjie@zmeng123.com>
2016-03-03 11:56:49 +0800

    test

--- PigCms/Lib/Action/Live/CommentAction.class.php
+++ PigCms/Lib/Action/Live/CommentAction.class.php
@@ -22,11 +22,36 @@ class CommentAction extends LiveAction
      */
     public function index ()
     {
-        $uris = array();
+        $uris = array(
+            'addUrl' => U('add')
+        );
         $urls = array_merge($this->ajaxUrls, $uris);
         $type = $this->_get('type');
         $this->assign('urls', $urls);
         $this->assign('type', $type);
         $this->display();
+    }
+
+    /**
+     * 添加直播或评论
+     */
+    public function add () {
+        if(!IS_AJAX) _404('页面不存在!');
+
+        $result = array('errcode' => 1, 'msg' => '添加失败!');
+
+        //当前用户id
+        $userId = $this->userId;
+        //ajax传递的userId
+        $postUserId = $this->_post('userId');
+        if($userId != $postUserId){
+            $result = array('errcode' => 2, 'msg' => '非法用户!');
+            $this->ajaxReturn($result, 'JSON');
+        }
+
+        $data = $this->_post();
+
+        $this->ajaxReturn($data, 'JSON');
+
     }
 }
---
+++ tpl/Live/default/style/js/comment.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Nsinm on 16/3/3.
+ */
+
+var commemtAction = {
+    'addComment' : function(){
+        $('.weui_btn.weui_btn_primary').click(function(){
+            alert(1111);
+        });
+    }
+};
