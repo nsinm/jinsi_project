659dc1dd8a4c98cb4efd42264c249862b658c9cf
shiyingjie <shiyingjie@zmeng123.com>
2016-03-04 18:53:05 +0800

    test

--- PigCms/Lib/Action/User/TelecastAction.class.php
+++ PigCms/Lib/Action/User/TelecastAction.class.php
@@ -11,8 +11,36 @@
 
 class TelecastAction extends UserAction
 {
+    /**
+     * 显示用户管理页
+     */
     public function index ()
     {
         $this->display();
+    }
+
+    /**
+     * 获取用户列表
+     */
+    public function getUserList ()
+    {
+        $this->_to404();
+
+        $result = array('errcode' => 1, 'msg' => '获取用户列表失败!');
+
+        $userList = M('user', 'jinsi_')->select();
+        if($userList){
+            $result = array('errcode' => 0, 'msg' => '获取用户列表成功!', 'data' => $userList);
+        }
+
+        $this->ajaxReturn($result, 'JSON');
+    }
+
+    /**
+     * 非ajax请求错误提示
+     */
+    private function _to404 ()
+    {
+        if(!IS_AJAX) _404('该页面不存在!');
     }
 }
--- tpl/User/default/common/js/telecast/index.js
+++ tpl/User/default/common/js/telecast/index.js
@@ -13,9 +13,49 @@ var indexAction = {
         });
     },
 
+    'getUserList' : function(){
+        var tag = $('#t');
+        $.getJSON(params.getUserListUrl, {}, function(data){
+            var html = '';
+            if(data.errcode == '0'){
+                var infos = data.data;
+                for(var index in infos){
+                    html += '<tr>';
+                    html +=     '<td><input type="checkbox" value="" class="cbitem" name="id[]"></td>';
+                    html +=     '<td>' + infos[index].id + '</td>';
+                    html +=     '<td>' + infos[index].open_id + '</td>';
+                    html +=     '<td>' + infos[index].jinsi_user_name + '</td>';
+                    html +=     '<td>' + infos[index].jinsi_user_header_pic + '</td>';
+                    if(infos[index].jinsi_user_type == '1'){
+                        html += '<td>普通用户</td>';
+                    }else{
+                        html += '<td>导师</td>';
+                    }
+                    html +=     '<td>' + infos[index].jinsi_user_style + '</td>';
+                    html +=     '<td>' + infos[index].jinsi_user_sign + '</td>';
+                    html +=     '<td>' + infos[index].jinsi_user_info + '</td>';
+                    if(infos[index].jinsi_user_recommend == '1'){
+                        html += '<td>推荐导师</td>';
+                    }else{
+                        html += '<td>普通导师</td>';
+                    }
+                    html +=     '<td>' + infos[index].jinsi_user_create_time + '</td>';
+                    html +=     '<td class="norightborder">';
+                    html +=         '<a href="javascript:void(0)">删除</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="javascript:void(0)">设为导师</a>';
+                    html +=     '</td>';
+                    html += '</tr>';
+                }
+            }
+            tag.html(html);
+        }, 'JSON');
+    },
+
     'init' : function(){
         //导航条点击事件
         this.navEvent();
+        if(params.tplName == 'user_list'){
+            this.getUserList();
+        }
     }
 };
 
