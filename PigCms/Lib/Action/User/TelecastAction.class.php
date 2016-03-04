28bd090e64adea29ccf412f3bbd04dc754f26a87
shiyingjie <shiyingjie@zmeng123.com>
2016-03-04 15:12:40 +0800

    增加直播后台

---
+++ PigCms/Lib/Action/User/TelecastAction.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * 直播后台控制器类
+ *
+ * Created by PhpStorm.
+ * User: Nsinm
+ * Date: 16/3/4
+ * Time: 下午2:22
+ */
+
+class TelecastAction extends UserAction
+{
+    public function index ()
+    {
+        $this->display();
+    }
+}
