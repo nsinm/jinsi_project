fd5c9fbd2cdae9d786e1b432cf24610ad0801f4b
shiyingjie <shiyingjie@zmeng123.com>
2016-03-06 15:30:08 +0800

    test

--- tpl/User/default/common/js/telecast/index.js
+++ tpl/User/default/common/js/telecast/index.js
@@ -41,7 +41,7 @@ var indexAction = {
                     }
                     html +=     '<td>' + infos[index].jinsi_user_city + '</td>';
                     html +=     '<td class="norightborder">';
-                    html +=         '<a href="javascript:void(0)">删除</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="javascript:void(0)">设为导师</a>';
+                    html +=         '<a href="javascript:void(0)">删除</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="javascript:void(0)">设为导师</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="javascript:void(0)">推荐</a>';
                     html +=     '</td>';
                     html += '</tr>';
                 }
