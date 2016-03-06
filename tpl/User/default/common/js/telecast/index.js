554d186fb0d782dd2b6882cff2809c144c4b2b74
shiyingjie <shiyingjie@zmeng123.com>
2016-03-07 01:39:53 +0800

    test

--- tpl/User/default/common/js/telecast/index.js
+++ tpl/User/default/common/js/telecast/index.js
@@ -11,6 +11,7 @@ var indexAction = {
                 $(this).addClass('current');
                 var text = $('a', $(this)).text();
                 $(".ListProduct").empty();
+                $('.M-box, .M-box1, .M-box2').empty();
                 switch (text){
                     case '用户管理':
                         indexAction.userPagination();
