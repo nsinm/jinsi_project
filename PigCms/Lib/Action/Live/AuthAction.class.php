ed9301fc0f27df82233fa0b0ff88624ea8dbbc30
phpunion <huangtuo@zmeng123.com>
2016-03-10 17:37:58 +0800

    调试发送消息模板

--- PigCms/Lib/Action/Live/AuthAction.class.php
+++ PigCms/Lib/Action/Live/AuthAction.class.php
@@ -19,14 +19,15 @@ class AuthAction extends Action
         $jssdk = D('Jssdk');
         $signPackage = $jssdk->GetSignPackage();
         //print_r($signPackage);
-        $this->assign('signPackage',$signPackage);
+        //$this->assign('signPackage',$signPackage);
         $data['title'] = "测试测试";
         $data['imgUrl'] = "https://www.baidu.com/img/baidu_jgylogo3.gif";
         $data['link'] = get_url();
-        $this->assign('data',$data);
+        //$this->assign('data',$data);
         $send_data['openid'] = "o6Kftv49cTfM5sQfnCmp6_kyP_48";
         $send_data['auther'] = "我是12哥";
         $send_data['content'] = "有新的股票发布了";
+        $send_data['url'] = "http://www.baidu.com";
         $rs = $jssdk->send_message($send_data);
         print_r($rs);
         $this->display();
