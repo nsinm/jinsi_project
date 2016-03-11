0486065fe4699ad1d3099fd8f49d86ef24914830
shiyingjie <shiyingjie@zmeng123.com>
2016-03-11 18:18:23 +0800

    test

--- tpl/Live/default/style/js/comment.js
+++ tpl/Live/default/style/js/comment.js
@@ -25,7 +25,7 @@ var commemtAction = {
             //评论人id
             var userId = params.userId;
             var push = 0;
-            if(confirm('是否推送给粉丝') && !isComment){
+            if(!isComment && confirm('是否推送给粉丝')){
                 push = 1;
             }
             var json = {'content':content, 'picUrl':picUrl, 'type':type, 'cid':cid, 'isComment':isComment, 'userId':userId, 'push':push};
