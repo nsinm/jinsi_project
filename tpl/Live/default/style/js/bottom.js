270c50e48bae07f202eacf36a44c9e25ca0b6dd1
shiyingjie <shiyingjie@zmeng123.com>
2016-03-16 16:41:23 +0800

    test

--- tpl/Live/default/style/js/bottom.js
+++ tpl/Live/default/style/js/bottom.js
@@ -8,11 +8,11 @@ $(function(){
             $('.bottom-bar div').removeClass('selected');
             $(this).attr('class', 'selected');
             if($(this).text() == '直播间'){
-                location.href = params.liveRoomUrl;
+                location.href = params.liveRoomUrl + '&random =' + Math.random();
             }else if($(this).text() == '导师'){
-                location.href = params.instructorUrl;
+                location.href = params.instructorUrl + '&random =' + Math.random();
             }else{
-                location.href = params.myUrl;
+                location.href = params.myUrl + '&random =' + Math.random();
             }
         })
 
