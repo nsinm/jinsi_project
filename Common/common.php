1caa59eb24f5b9d758e5772e42d71da2ed65f644
shiyingjie <shiyingjie@zmeng123.com>
2016-03-18 11:58:33 +0800

    test

--- Common/common.php
+++ Common/common.php
@@ -677,4 +677,31 @@ if(!function_exists('p')) {
         print_r($arr);
     }
 }
+
+function utf8_substr($str,$position,$length){
+    $start_position = strlen($str);
+    $start_byte = 0;
+    $end_position = strlen($str);
+    $count = 0;
+    for($i = 0; $i < strlen($str); $i++){
+        if($count >= $position && $start_position > $i){
+            $start_position = $i;
+            $start_byte = $count;
+        }
+        if(($count-$start_byte)>=$length) {
+            $end_position = $i;
+            break;
+        }
+        $value = ord($str[$i]);
+        if($value > 127){
+            $count++;
+            if($value >= 192 && $value <= 223) $i++;
+            elseif($value >= 224 && $value <= 239) $i = $i + 2;
+            elseif($value >= 240 && $value <= 247) $i = $i + 3;
+            else die('Not a UTF-8 compatible string');
+        }
+        $count++;
+    }
+    return(substr($str,$start_position,$end_position-$start_position));
+}
 ?>
