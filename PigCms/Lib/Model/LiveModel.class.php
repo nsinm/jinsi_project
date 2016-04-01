adbcd2ed8ce763fd8d1899a47156dbdf24e5fc34
shiyingjie <shiyingjie@zmeng123.com>
2016-04-01 10:59:49 +0800

    test

--- PigCms/Lib/Model/LiveModel.class.php
+++ PigCms/Lib/Model/LiveModel.class.php
@@ -181,8 +181,8 @@ class LiveModel extends Model
         }
         $data['id'] = $id;
         $data['push'] = 2;
-        if($flag)
-            $content->save($data);
+//        if($flag)
+//            $content->save($data);
         //print_r($follow_list);
 
 
