742534320a7a1fdcd6c7dc235d75661b909cefdf
phpunion <huangtuo@zmeng123.com>
2016-04-18 18:16:10 +0800

    引入微信支付方法

--- wxpay/demo/js_api_call.php
+++ wxpay/demo/js_api_call.php
@@ -62,8 +62,8 @@ header("Content-type: text/html; charset=utf-8");
         //exit;
     }
 	$unifiedOrder->setParameter("total_fee",$total_fee);//总金额
-	$unifiedOrder->setParameter("notify_url",WxPayConf_pub::NOTIFY_URL);//通知地址
-    //$unifiedOrder->setParameter("notify_url","http://mp.jinsxy.com/index.php?g=Live&m=Auth&a=get_pay_info");//通知地址
+	//$unifiedOrder->setParameter("notify_url",WxPayConf_pub::NOTIFY_URL);//通知地址
+    $unifiedOrder->setParameter("notify_url","http://mp.jinsxy.com/index.php?g=Live&m=Auth&a=get_pay_info");//通知地址
 	$unifiedOrder->setParameter("trade_type","JSAPI");//交易类型
 	//非必填参数，商户可根据实际情况选填
 	//$unifiedOrder->setParameter("sub_mch_id","XXXX");//子商户号  
