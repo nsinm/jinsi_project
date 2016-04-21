9af6729d4805d960e01fdc5d5d29b20cbe483a0c
shiyingjie <shiyingjie@zmeng123.com>
2016-04-21 17:09:02 +0800

    test

--- tpl/Livetest/default/style/js/user.js
+++ tpl/Livetest/default/style/js/user.js
@@ -52,7 +52,7 @@ var userAction = {
                         }else{
                             html +=     '<a href="javascript:;" class="weui_btn weui_btn_plain_primary jumpBt attention" data-value="' + infos[index].id + '">关注</a>';
                         }
-                        if(infos[index].is_member != '0'){
+                        if(infos[index].is_member != '0' || params.userId == infos[index].user_id){
                             html +=         '<a href="javascript:;" class="weui_btn weui_btn_mini weui_btn_default jumpBt c_member">成为会员</a>';
                         }else{
                             html +=         '<a href="javascript:;" class="weui_btn weui_btn_plain_primary jumpBt member" data-value="' + infos[index].id + '" data-filter="' + infos[index].is_follow + '" data-name="' + infos[index].jinsi_user_name + '">成为会员</a>';
@@ -95,12 +95,8 @@ var userAction = {
                         });
                     }else  if($(this).attr('class').indexOf('member') > 0){
                         var insName = $(this).attr('data-name');
-                        var isFollow = $(this).attr('data-filter');
+                        //var isFollow = $(this).attr('data-filter');
                         $(this).click(function(){
-                            if('0' == isFollow){
-                                alert('请先关注该导师!');
-                                return;
-                            }
                             location.href = params.payUrl + '&userId=' + params.userId + '&fid=' + instructorId + '&insName=' + insName;
                         })
                     }
