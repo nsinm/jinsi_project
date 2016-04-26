9d7318a6a61ea519b8ab732b07fbc580774846be
phpunion <huangtuo@zmeng123.com>
2016-04-26 20:55:47 +0800

    测试是否支持表情

---
+++ wxpay/end.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: huangtuo
+ * Date: 16/4/26
+ * Time: ÏÂÎç8:51
+ */
+$lnk = mysql_connect('rdsvda87txg713u30yiq.mysql.rds.aliyuncs.com', 'jinsi', 'Jinsi123654')
+or die ('Not connected : ' . mysql_error());
+
+// make foo the current db
+mysql_select_db('jinsi2', $lnk) or die ('Cant use foo : ' . mysql_error());
+
+mysql_query("set names 'utf9mb4'");
+$value = $_POST['content'];
+
+$sql = "INSERT INTO `jinsi_emoji` (`id`, `content`) VALUES (NULL, '{$value}');";
+
+$rs = mysql_query($sql);
+print_r($rs);
---
+++ wxpay/test.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Untitled Document</title>
+</head>
+
+<body>
+<form name="frm" method="post" action="end.php">
+    <textarea name="content"></textarea>
+    <input type="button">
+</form>
+</body>
+</html>
