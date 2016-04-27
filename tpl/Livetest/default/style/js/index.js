33a8d22438a6c2f1955b6c701cc669e1c9ea289d
shiyingjie <shiyingjie@zmeng123.com>
2016-04-27 12:30:35 +0800

    test

--- tpl/Livetest/default/style/js/index.js
+++ tpl/Livetest/default/style/js/index.js
@@ -152,15 +152,15 @@ var indexAction = {
                         html +=         '<span class="like-btn">';
                         html +=              '已阅&nbsp;' + parseInt(infos[index].jinsi_content_read_no) * 13;
                         html +=         '</span>';
-                        html +=         '<span id="icon-comment" style="margin-left:8px;">';
+                        html +=         '<span id="icon-comment" style="margin-left:12px;">';
                         html +=             '<span class="icon-comment" alt="">';
                         html +=             '</span>评论&nbsp;' + infos[index].jinsi_content_comment_no;
                         html +=         '</span>';
-                    html +=             '<span class="comment" style="margin-left:8px;">';
+                    html +=             '<span class="comment" style="margin-left:12px;">';
                     html +=                 '<span class="icon-reward" alt="">';
                     html +=                 '</span>打赏';
                     html +=             '</span>';
-                        html +=         '<span style="margin-left:8px;">';
+                        html +=         '<span style="margin-left:12px;">';
                     if(infos[index].current_user_praise == 1){
                         html +=             '<span class="icon-like" alt="">';
                     }else{
